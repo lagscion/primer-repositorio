3441232d84f4eb06df0addf986f5e19c65b5ac1d
lagscion <76491526+lagscion@users.noreply.github.com>
2026-01-29 00:10:03 -0500

    nequi_entero.py

--- nequi.py
+++ nequi.py
@@ -11,9 +11,16 @@ def main ():
         elif op == 2:
             saldo = saldo - paga(saldo)
         elif op == 3:
-            transferir ():
+            saldo = saldo - transferir (saldo)
+        elif op == 4:
+            mostrar (saldo)
 
     print ("gracias por usar nequi")
+
+
+
+def mostrar (saldo):
+    print (saldo)
 
 
 def monto ():
@@ -39,42 +46,27 @@ def paga (saldo):
     return int (paga)
 
 
-# def transferir (saldo):
-#     num_dest = (input("ingrese el numero del destinatario: "))
-#     transerencia = int(input("ingrese el valor de la transferencia: "))
-#     while (len(num_dest)) != 10   :
-#         print ("Error. Digite un NUMERO valido. ")
-#         input ("presione cualquier tecla para continuar....")
-#         print ("\n\n")
-#         menu()
-#         transerencia = input()
-#     while int (transerencia) <=0 or transerencia > saldo:
-#         print ("Error. Digite un valor valido. ")
-#         input ("presione cualquier tecla para continuar....")
-#         print ("\n\n")
-#         menu()
-#         transerencia = input()
-#     return 
-
-
-
-
 def transferir (saldo):
     num_dest = (input("ingrese el numero del destinatario: "))
-    transerencia = int(input("ingrese el valor de la transferencia: "))
-    while (len(num_dest)) != 10   :
+    transferencia = (input("ingrese el valor de la transferencia: "))
+
+    while  (not num_dest.isdigit()) or len(num_dest) != 10:  
         print ("Error. Digite un NUMERO valido. ")
         input ("presione cualquier tecla para continuar....")
         print ("\n\n")
         menu()
-        transerencia = input()
-    while int (transerencia) <=0 or transerencia > saldo:
-        print ("Error. Digite un valor valido. ")
+        num_dest= input()
+    while (not transferencia.isdigit()) or int (transferencia) <=0 or int(transferencia) > saldo:
+        print ("Error. saldo insuficiente. ")
         input ("presione cualquier tecla para continuar....")
         print ("\n\n")
         menu()
-        transerencia = input()
-    return 
+        transferencia = input()
+    return int (transferencia)
+
+
+
+
 
 
 
@@ -96,13 +88,13 @@ def menu():
 5. Salir
 
 --------
-OPCION (1-4)?""")
+OPCION (1-5)?""")
     
 
 def leer_opcion():
     op =  (input("digite su opcion: "))
     while ( not op.isdigit() ) or int (op) < 1 or int (op) > 5:
-        print ("Error. Digite un numero del 1 a el 3. ")
+        print ("Error. Digite un numero del 1 a el 5. ")
         input ("presione cualquier tecla para continuar....")
         print ("\n\n")
         menu()
